65345e38ef0aab3f0c6dab35bb09cd51fcd3f0eb
Rishabh Tripathi <rishabhtripathi587@gmail.com>
2024-02-02 23:54:58 +0530

    update : product model

--- models/product.model.js
+++ models/product.model.js
@@ -1,16 +1,23 @@
 const mongoose = require("mongoose");
 
-const productSchema = mongoose.Schema(
+// const colorsSchema = new mongoose.Schema({
+//   color: { type: String, required: true },
+//   code: { type: String, required: true },
+// });
+const productSchema =new mongoose.Schema(
   {
     category: { type: String, required: true },
     title: { type: String, required: true },
     price: { type: Number, required: true },
     images: { type: Array, required: true },
-    colors: { type: Array },
+    colors:  [{name:String,code:String}],
   },
   { versionKey: false }
 );
-
+/**
+ * const cellSchema = new mongoose.Schema({type: String,count: Number});
+const matrixSchema = new mongoose.Schema({matrix: [[cellSchema]]});
+ */
 const ProductModel = mongoose.model("product", productSchema);
 
 module.exports = { ProductModel };
