53cc2560d2f966e61d2c6466735c03c98c5b7d39
Rishabh Tripathi <rishabhtripathi587@gmail.com>
2024-02-02 13:27:55 +0530

    added : product model

---
+++ models/product.model.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+
+const productSchema = mongoose.Schema(
+  {
+    category: { type: String, required: true },
+    title: { type: String, required: true },
+    price: { type: Number, required: true },
+    images: { type: Array, required: true },
+    colors: { type: Array },
+  },
+  { versionKey: false }
+);
+
+const ProductModel = mongoose.model("product", productSchema);
+
+module.exports = { ProductModel };
