9abebcc39c8414287da0f0f7f15356e4496e93fa
Rishabh Tripathi <rishabhtripathi587@gmail.com>
2024-02-02 23:58:47 +0530

    removed : some commeted lines

--- models/product.model.js
+++ models/product.model.js
@@ -1,9 +1,5 @@
 const mongoose = require("mongoose");
 
-// const colorsSchema = new mongoose.Schema({
-//   color: { type: String, required: true },
-//   code: { type: String, required: true },
-// });
 const productSchema =new mongoose.Schema(
   {
     category: { type: String, required: true },
@@ -14,10 +10,6 @@ const productSchema =new mongoose.Schema(
   },
   { versionKey: false }
 );
-/**
- * const cellSchema = new mongoose.Schema({type: String,count: Number});
-const matrixSchema = new mongoose.Schema({matrix: [[cellSchema]]});
- */
 const ProductModel = mongoose.model("product", productSchema);
 
 module.exports = { ProductModel };
