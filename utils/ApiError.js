8e92da453179714f591a93c771b95a2bd44a1cec
Rishabh Tripathi <rishabhtripathi587@gmail.com>
2024-02-04 10:47:11 +0530

    feat: add error discreption for client

--- utils/ApiError.js
+++ utils/ApiError.js
@@ -1,8 +1,9 @@
 class ApiError extends Error {
-    constructor(statusCode, message) {
+    constructor(statusCode, message, error) {
         super(message);
         this.success = false;
         this.statusCode = statusCode;
+        error ? this.error = error : null
     }
 }
 
