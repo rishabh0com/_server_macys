616bb46673d4bcd8b010b905e41ad4ac0b7742fa
Rishabh Tripathi <rishabhtripathi587@gmail.com>
2024-02-09 09:24:36 +0530

    fix: cors policy

--- index.js
+++ index.js
@@ -13,17 +13,24 @@ const app = express();
 // get request for homepage :
 app.get("/", async (req, res) => {
   try {
-    if(req) throw new ApiError(400, "Bad request");
+    if (req) throw new ApiError(400, "Bad request");
     res.send(new ApiResponse(200, null, "Welcome to the homepage"));
   } catch (error) {
     console.log("Error", error);
-    res.status(500).send({ message: error.message,...error });
+    res.status(500).send({ message: error.message, ...error });
   }
 });
 
 // middleware
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+
+  origin : ["*","http://localhost:5173"],
+    credentials: true,
+}));
+
+// // this origin and credentials is used to allow the frontend to access the backend
+// app.use(cors({origin:["http://localhost:5173"],credentials:true}));
 app.use(cookieParser());
 
 // Routes
